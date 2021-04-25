473f1e59e4eb410a89509a65534afe71f50baffd
LyudmilaRomanchenko <78047438+LyudmilaRomanchenko@users.noreply.github.com>
2021-04-25 03:12:49 +0300

    Добавляет секцию customer reviews

---
+++ src/customer-reviews.js
@@ -0,0 +1,17 @@
+$('.slide-reviews').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slide-customer',
+    autoplay:true
+     });
+   $('.slide-customer').slick({
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   asNavFor: '.slide-reviews',
+   dots: true,
+   centerMode: true,
+   focusOnSelect: true
+   });
