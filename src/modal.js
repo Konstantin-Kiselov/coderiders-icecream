55ce71b088cefce400cecb07a339c2fdf677f562
Valerii Kurochka <kurochka.valerii@gmail.com>
2021-04-26 13:22:53 +0300

    сделана модалка about

--- src/modal.js
+++ src/modal.js
@@ -1,8 +1,26 @@
+// buy now
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle('backdrop-is-hidden');
+  }
+})();
+
+// read more
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-about-open]'),
+    closeModalBtn: document.querySelector('[data-modal-about-close]'),
+    modal: document.querySelector('[data-modal-about]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
