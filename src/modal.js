ff88a8d74e908801c6c0113b41ec48c9a4dab25b
Valerii Kurochka <kurochka.valerii@gmail.com>
2021-04-26 16:41:33 +0300

    оформлена модалка franchise

--- src/modal.js
+++ src/modal.js
@@ -48,3 +48,20 @@
     refs.modal.classList.toggle('backdrop-is-hidden');
   }
 })();
+
+// Franchise
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-franchise-open]'),
+    closeModalBtn: document.querySelector('[data-modal-franchise-close]'),
+    modal: document.querySelector('[data-modal-franchise]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle('backdrop-is-hidden');
+  }
+})();
